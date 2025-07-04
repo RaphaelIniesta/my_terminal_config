ff3e947716d0bca44037dbfce2420c8c15814665
RaphaelIniesta <raphael_iniesta@yahoo.com>
2025-07-04 18:05:24 -0300

    update beautify.sh

--- beautify.sh
+++ beautify.sh
@@ -5,8 +5,8 @@ fi
 
 brew install thefuck
 
-fastfetch --gen-config
 > ~/.config/fastfetch/config.jsonc
+fastfetch --gen-config
 mkdir ~/.config/fastfetch/ASCII
 curl https://raw.githubusercontent.com/RaphaelIniesta/my_terminal_config/refs/heads/main/config.jsonc >> ~/.config/fastfetch/config.jsonc
 curl https://raw.githubusercontent.com/RaphaelIniesta/my_terminal_config/refs/heads/main/ASCII/aiq.txt >> ~/.config/fastfetch/ASCII/aiq.txt
